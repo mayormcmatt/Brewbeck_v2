eab920b09701ae2675e873685c29223f888af108
mayormcmatt <matthew.is.riding@gmail.com>
2015-11-19 23:12:38 -0800

    app scaffolded

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Brewbeck, $*/
+
+
+window.Brewbeck = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Brewbeck.init();
+});
